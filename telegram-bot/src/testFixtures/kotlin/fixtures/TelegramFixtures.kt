d0ffee62f91c627c1cc767231b4a664448c27a98
evmetatron <artem.nagibin.89@ya.ru>
2022-09-30 23:08:07 +0300

    Remove duplicate comments

--- telegram-bot/src/testFixtures/kotlin/fixtures/TelegramFixtures.kt
+++ telegram-bot/src/testFixtures/kotlin/fixtures/TelegramFixtures.kt
@@ -3,11 +3,6 @@
  * Репозиторий приложения: https://github.com/evmetatron/EvFunnyTestBot
  */
 
-/*
- * Код написан разработчиком evmetatron (artem.nagibin.89@ya.ru)
- * Репозиторий приложения: https://github.com/evmetatron/EvFunnyTestBot
- */
-
 package fixtures
 
 import org.telegram.telegrambots.meta.api.objects.Message
